57dc1e8cb7aa9f31dbc6d1c563c0769e0ff8915a
Ebenezer <ebenezeryeboah46@gmail.com>
2025-03-18 15:17:50 +0000

    read-more

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ const readMoreText = document.querySelector('.read-more-text');
 const readMoreButton = document.querySelector('.read-more-button');
 
 readMoreButton.addEventListener('click', () => {
-    if (readMoreText.classList.contains('read-more-text')) {
-        readMoreText.classList.remove('read-more-text');
+    if (readMoreText.classList.contains('reveal')) {
+        readMoreText.classList.remove('reveal');
+        readMoreButton.textContent = "| READ MORE |";
 
-    } else {
-        readMoreText.classList.add('read-more-text');
+    }
+    else {
+        readMoreText.classList.add('reveal');
+        readMoreButton.textContent = "| READ LESS |"
     }
 })
