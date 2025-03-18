38d9760c5b239e2b418322270b511b1cc53e4726
Ebenezer <ebenezeryeboah46@gmail.com>
2025-03-18 16:06:50 +0000

    read more solved

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const readMoreText = document.querySelector('.read-more-text');
 
 const readMoreButton = document.querySelector('.read-more-button');
+let isRevealed = false
 
-readMoreButton.addEventListener('click', () => {
-    if (readMoreText.classList.contains('reveal')) {
-        readMoreText.classList.remove('reveal');
-        readMoreButton.textContent = "| READ MORE |";
+readMoreText.classList.add('reveal');
+
+readMoreButton.addEventListener(
+    'click', () => {
+        if (isRevealed) {
+            readMoreText.classList.add('reveal');
+            readMoreButton.textContent = '| READ MORE |';
+        } else {
+            readMoreText.classList.remove('reveal');
+            readMoreButton.textContent = '| READ LESS |';
+
+        }
+        isRevealed = !isRevealed
 
     }
-    else {
-        readMoreText.classList.add('reveal');
-        readMoreButton.textContent = "| READ LESS |"
-    }
-})
+)
